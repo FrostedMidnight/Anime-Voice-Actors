bb6aac6e64e070f0dc43d64868d3c3cfee8a3805
FrostedMidnight <hammerkirby11@gmail.com>
2025-01-05 16:30:41 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Character:
+    def __init__(self, name, anime, en_va, jp_va):
+        self.name = name
+        self.anime = anime
+        self.en_va = en_va
+        self.jp_va = jp_va
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
