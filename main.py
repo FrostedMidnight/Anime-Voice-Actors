d2735aeb25bd22de7fd399eff852274a90cf4384
Justyce Goode <frostedmidnight4@proton.me>
2025-01-19 17:34:34 -0500

    Revert "Removed colors"
    This reverts commit 01f495feaa03059d55d72e1b7141b529f7523cc5.

--- main.py
+++ main.py
@@ -30,5 +30,8 @@ def main():
         Character("Xiaolan", "The Apothecary Diaries", "N/A", "Misaki Kuno")
     ]
 
+    #My favorite color is blue.
+    #My favorite color is red.
+
 if __name__ == "__main__":
     main()
