9700b9880504695842db526729e4ed1d812a830d
Justyce Goode <frostedmidnight4@proton.me>
2025-01-19 19:43:28 -0500

    Overhaul

--- main.py
+++ main.py
@@ -1,52 +1,65 @@
 import pandas as pd
 
-# class Character:
-#     def __init__(self, name, anime, en_va, jp_va):
-#         self.name = name
-#         self.anime = anime
-#         self.en_va = en_va
-#         self.jp_va = jp_va
-
 def find_by_en_va(df, name):
     print(df[df["English VA"] == name])
 
 def main():
-    # character_list = [
-    #     Character("Index", "A Certain Magical Index", "Monical Rial", "Yuka Iguchi"),
-    #     Character("Mikoto Misaka", "A Certain Magical Index", "Brittney Karbowski", "Rina Satou"),
-    #     Character("Kaguya Yami", "Date A Live", "Brittney Karbowski", None),
-    #     Character("Yoshino", "Date A Live", "Tia Ballard", None),
-    #     Character("Yue", "Arifureta", "Tia Ballard", None),
-    #     Character("Hajime Nogumo", "Arifureta", "Matt Shipman", None),
-    #     Character("Kouki Amanogawa", "Arifureta", "Ricco Fajardo", None),
-    #     Character("Chrome", "Dr. Stone", "Matt Shipman", None),
-    #     Character("Taiju", "Dr. Stone", "Ricco Fajardo", None),
-    #     Character("Leen", "In Another World With My Smartphone", "Monical Rial", "Sumire Uesaka"),
-    #     Character("Charlotte", "Parallel World Pharmacy", "Lindsay Siedel", None),
-    #     Character("Kirisame", "Dr. Stone", "Lindsay Siedel", None),
-    #     Character("Sense", "Frieren: Beyond Journey's End", "Lindsay Siedel", None),
-    #     Character("Irisphilia", "Am I Actually the Strongest", "Linday Siedel", None),
-    #     Character("Shio", "Happy Sugar Life", "N/A", "Misaki Kuno"),
-    #     Character("Klee", "Genshin Impact", None, "Misaki Kuno"),
-    #     Character("Hu Tao", "Genshin Impact", "Brinanna Knickerbocker", None),
-    #     Character("Lily", "Arifureta", "Brinanna Knickerbocker", "Noriko Shibasaki"),
-    #     Character("Filo", "The Rising of the Shield Hero", "Brinanna Knickerbocker", "Rina Hidaka"),
-    #     Character("Satou", "Happy Sugar Life", "N/A", "Kana Hanazawa"),
-    #     Character("Xiaolan", "The Apothecary Diaries", "N/A", "Misaki Kuno")
-    # ]
+
+    female_en_va_list = [
+        "Monical Rial",
+        "Brittney Karbowski",
+        "Tia Ballard",
+        "Brinanna Knickerbocker",
+        "Lindsay Siedel",
+        "Sarah Wiedenheft"
+    ]
+
+    male_en_va_list = [
+        "Matt Shipman",
+        "Ricco Fajardo"
+    ]
+
+    female_jp_va_list = [
+        "Misaki Kuno",
+        "Kaede Kondo",
+        "Rie Takahashi"
+    ]
 
     va_data = [
         ["Index", "A Certain Magical Index", "Yuka Iguchi", "Monical Rial"],
         ["Mikoto Misaka", "A Certain Magical Index", "Rina Satou", "Brittney Karbowski"],
-        ["Kaguya Yami", "Date A Live", None , "Brittney Karbowski"]
+        ["Kaguya Yami", "Date A Live", "Maaya Uchida", "Brittney Karbowski"],
+        ["Yoshino", "Date A Live", "Iori Nomizu", "Tia Ballard"],
+        ["Yue", "Arifureta", "Yuuki Kuwahara", "Tia Ballard"],
+        ["Hajime Nogumo", "Arifureta", "Toshinari Fukamachi", "Matt Shipman"],
+        ["Kouki Amanogawa", "Arifureta", "Tetsuya Kakihara", "Ricco Fajardo"],
+        ["Chrome", "Dr. Stone", "Ren Satou", "Matt Shipman"],
+        ["Taiju", "Dr. Stone", "Makoto Furukawa", "Ricco Fajardo"],
+        ["Leen", "In Another World With My Smartphone", "Sumire Uesaka", "Monical Rial"],
+        ["Charlotte", "Parallel World Pharmacy", "Kaede Hondo", "Lindsay Siedel"],
+        ["Kirisame", "Dr. Stone", "Kaeda Hondo", "Lindsay Siedel"],
+        ["Sense", "Frieren: Beyond Journey's End", "Haruka Terui", "Lindsay Siedel"],
+        ["Irisphilia", "Am I Actually the Strongest", "Ami Koshimizu", "Linday Siedel"],
+        ["Shio", "Happy Sugar Life", "Misaki Kuno", "N/A"],
+        ["Klee", "Genshin Impact", "Misaki Kuno", "Poonam Basu"],
+        ["Hu Tao", "Genshin Impact", "Rie Takahashi", "Brinanna Knickerbocker"],
+        ["Lily", "Arifureta", "Noriko Shibasaki", "Brinanna Knickerbocker"],
+        ["Filo", "The Rising of the Shield Hero", "Rina Hidaka", "Brinanna Knickerbocker"],
+        ["Satou", "Happy Sugar Life", "Kana Hanazawa", "N/A"],
+        ["Xiaolan", "The Apothecary Diaries", "Bree Han", "Misaki Kuno"],
+        ["Flora", "Spirit Chronicles", "Kaeda Hondo", "Madeleine Morris"],
+        ["Hakari", "The 100 Girlfriends", "Kaede Hondo", "Sarah Wiedenheft"],
+        ["Laika", "Slime 300", "Kaeda Hondo", "Rachelle Heger"],
+        ["Hifumi", "Blue Archive", "Kaeda Hondo", "N/A"],
+        ["Suika", "Dr. Stone", "Karin Takahasi", "Sarah Wiedenheft"],
+        ["Miledi", "Arifureta", "Yukana", "Sarah Wiedenheft"]
     ]
 
     va_df = pd.DataFrame(va_data, columns = ["Name", "Anime", "Japanese VA", "English VA"])
 
-    #print(va_df)
-    #print(va_df[va_df["English VA"] == "Brittney Karbowski"])
+    chosen_name = input("Enter an english va: ")
 
-    find_by_en_va(va_df, "Brittney Karbowski")
+    find_by_en_va(va_df, chosen_name)
 
 if __name__ == "__main__":
     main()
